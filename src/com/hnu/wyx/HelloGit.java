56ee48f648d4fda29db71af2083d01900b44fe94
yxwang713 <wangyanxiang713@gmail.com>
2013-03-30 16:12:09 +0800

    四大发明

--- src/com/hnu/wyx/HelloGit.java
+++
@@ -1,5 +0,0 @@
-package com.hnu.wyx;
-
-public class HelloGit {
-
-}
---
+++ src/com/hnu/wyx/Test.java
@@ -0,0 +1,13 @@
+package com.hnu.wyx;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdf");
+	}
+
+}
